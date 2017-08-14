fbbaeeb01cb3eb9fe38a171cc0d1114d9f91501f
dengweijie <dengweijie@lizhi.fm>
2017-08-14 17:03:06 +0800

    changed: add empty class remove Test&UITest target

---
+++ CustomEmoticon/Empty1/EmptyClass.m
@@ -0,0 +1,13 @@
+//
+//  EmptyClass.m
+//  CustomEmoticon
+//
+//  Created by dengweijie on 2017/8/14.
+//  Copyright © 2017年 XHJ. All rights reserved.
+//
+
+#import "EmptyClass.h"
+
+@implementation EmptyClass
+
+@end
